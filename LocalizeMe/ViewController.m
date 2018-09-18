18efadd66065f1656c29895ae7ae8bf1c3ad377b
Alex Chekodanov <checkodanov@gmail.com>
2018-09-18 17:47:48 +0300

    Add fr language

--- LocalizeMe/ViewController.m
+++ LocalizeMe/ViewController.m
@@ -8,7 +8,11 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController()
+
+@property (weak, nonatomic) IBOutlet UILabel *localeLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *flagImageView;
+@property (strong, nonatomic) IBOutletCollection(UILabel) NSArray *labels;
 
 @end
 
@@ -17,6 +21,20 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    NSLocale *locale = [NSLocale currentLocale];
+    NSString *currentLangID = [[NSLocale preferredLanguages] objectAtIndex:0];
+    NSString *displayLang = [locale displayNameForKey:NSLocaleLanguageCode value:currentLangID];
+    NSString *capitalized = [displayLang capitalizedStringWithLocale:locale];
+    self.localeLabel.text = capitalized;
+    
+    [self.labels[0] setText:NSLocalizedString(@"LABEL_ONE", @"The number 1")];
+    [self.labels[1] setText:NSLocalizedString(@"LABEL_TWO", @"The number 2")];
+    [self.labels[2] setText:NSLocalizedString(@"LABEL_THREE", @"The number 3")];
+    [self.labels[3] setText:NSLocalizedString(@"LABEL_FOUR", @"The number 4")];
+    [self.labels[4] setText:NSLocalizedString(@"LABEL_FIVE", @"The number 5")];
+    
+    NSString *flagFile = NSLocalizedString(@"FLAG_FILE", @"Name of the flag");
+    self.flagImageView.image = [UIImage imageNamed:flagFile];
 }
 
 
